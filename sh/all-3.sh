ad2b60808e53ca1d3a6c2231bd7485ecafec2ddb
Albara Ramli <arramli@ucdavis.edu>
2019-05-01 19:33:53 -0700

    Add files via upload

---
+++ sh/all-3.sh
@@ -0,0 +1,14 @@
+touch ip.txt
+echo $(/sbin/ip -o -4 addr list wlp2s0 | awk '{print $4}' | cut -d/ -f1) > ip.txt
+ip=$(<ip.txt)
+if test -z "$ip"
+then
+    echo $(/sbin/ip -o -4 addr list enp0s3 | awk '{print $4}' | cut -d/ -f1) > ip.txt
+else
+    echo "N"
+fi
+#cd pantheon
+#gnome-terminal -e 'sh -c "python third_party/indigo/env/proxy.py 9999;exec bash"'
+#cd ../
+####gnome-terminal -e 'sh -c "sh ./pensieve-setup4-mahimahi.sh;exec bash"'
+#gnome-terminal -e 'sh -c "sh ./pensieve-setup5-real.sh;exec bash"'
